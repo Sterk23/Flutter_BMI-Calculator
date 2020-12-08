6812137a8014eb05b53ab9e518f0199e8ec79354
Thananon <thananon@digio.co.th>
2020-12-08 12:55:30 +0700

    Learn how to use 'Intent' in flutter (they called 'Route')

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -205,15 +205,21 @@ class _InputPageState extends State<InputPage> {
                 ],
               ),
             ),
-            Container(
-              color: kBottomContainerColor,
-              margin: EdgeInsets.only(top: 10.0),
-              width: double.infinity,
-              height: kBottomContainerHeight,
-              child: Center(
-                child: Text(
-                  'Calculate your BMI',
-                  style: kButtonTextStyle,
+            GestureDetector(
+              onTap: (){
+                Navigator.pushNamed(context, '/result');
+
+              },
+              child: Container(
+                color: kBottomContainerColor,
+                margin: EdgeInsets.only(top: 10.0),
+                width: double.infinity,
+                height: kBottomContainerHeight,
+                child: Center(
+                  child: Text(
+                    'Calculate your BMI',
+                    style: kButtonTextStyle,
+                  ),
                 ),
               ),
             )
